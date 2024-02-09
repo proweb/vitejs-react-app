160ac5008516eddb7cd3cf928fc808a5b2c593a1
proweb <proweb@users.noreply.github.com>
2024-02-09 13:04:32 +0300

    Add Layout Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import { FormElements } from './components/FormElements';
+import Layout from './components/Layout';
 
 export default function App() {
   return (
-    <>
+    <Layout>
       <h1 className="text-3xl">Modern React</h1>
-      <FormElements />
-    </>
+    </Layout>
   );
 }
---
+++ src/components/Layout.jsx
@@ -0,0 +1,3 @@
+export default function Layout({ children }) {
+  return <div className="container mx-auto px-4">{children}</div>;
+}
