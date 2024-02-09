4198b34cf8508d2c2a20bfc6d17f455d37cb7f30
proweb <proweb@users.noreply.github.com>
2024-02-09 12:32:46 +0300

    Add Tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { FormElements } from './components/FormElements';
 export default function App() {
   return (
     <>
-      <h1>Modern React</h1>
+      <h1 className="text-3xl">Modern React</h1>
       <FormElements />
     </>
   );
